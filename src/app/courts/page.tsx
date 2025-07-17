787e0dcf309942186acf5ad85eb9ad16d2e042fe
Wesley Lopes de Oliveira <x.wesleylopes@gmail.com>
2025-07-17 19:56:43 -0300

    feat: add search input and badge component to courts page for enhanced functionality

--- src/app/courts/page.tsx
+++ src/app/courts/page.tsx
@@ -1,16 +1,9 @@
-import { Edit, Plus, Trash } from 'lucide-react'
+import { Edit, Plus, SearchIcon, Trash } from 'lucide-react'
 
 import Navbar from '../components/Navbar'
 
 import { Button } from '@/components/ui/button'
-import {
-  Card,
-  CardAction,
-  CardContent,
-  CardDescription,
-  CardHeader,
-  CardTitle,
-} from '@/components/ui/card'
+import { Input } from '@/components/ui/input'
 
 import {
   Table,
@@ -20,27 +13,33 @@ import {
   TableHeader,
   TableRow,
 } from '@/components/ui/table'
+import { Badge } from '@/components/ui/badge'
 
 const courts = [
   {
     id: '1',
     name: 'Quadra 1',
+    isActive: true
   },
   {
     id: '2',
     name: 'Quadra 2',
+    isActive: true
   },
   {
     id: '3',
     name: 'Quadra 3',
+    isActive: true
   },
   {
     id: '4',
     name: 'Quadra 4',
+    isActive: false
   },
   {
     id: '5',
     name: 'Quadra 5',
+    isActive: true
   }
 ]
 
@@ -49,46 +48,53 @@ export default function Courts() {
     <div>
       <Navbar />
       <main className="max-w-7xl mx-auto mt-10">
-        <Card className="rounded-md">
-          <CardHeader>
-            <CardTitle className="text-xl">Quadras</CardTitle>
-            <CardDescription>
-              Aqui você pode visualizar, adicionar, editar e remover todas as quadras que deseja disponibilizar para aluguel.
-            </CardDescription>
-            <CardAction>
-              <Button>
-                <Plus /> Nova quadra
-              </Button>
-            </CardAction>
-          </CardHeader>
-          <CardContent>
-            <Table>
-              <TableHeader>
-                <TableRow className="hover:bg-transparent">
-                  <TableHead>#</TableHead>
-                  <TableHead>Nome</TableHead>
-                  <TableHead>Ações</TableHead>
-                </TableRow>
-              </TableHeader>
-              <TableBody>
-                {courts.map((court) => (
-                  <TableRow key={court.id}>
-                    <TableCell>{court.id}</TableCell>
-                    <TableCell>{court.name}</TableCell>
-                    <TableCell>
-                      <Button variant="secondary" size="icon" className="mr-2 size-8">
-                        <Edit />
-                      </Button>
-                      <Button variant="secondary" size="icon" className="size-8">
-                        <Trash />
-                      </Button>
-                    </TableCell>
-                  </TableRow>
-                ))}
-              </TableBody>
-            </Table>
-          </CardContent>
-        </Card>
+        <div className="flex items-center justify-between">
+          <h1 className="text-2xl font-semibold">Quadras</h1>
+          <Button>
+            <Plus /> Nova quadra
+          </Button>
+        </div>
+        <div className="mt-6">
+          <div className="relative w-full max-w-xs">
+            <Input
+              className="peer ps-9 pe-9"
+              placeholder="Pesquisar quadras..."
+              type="search"
+            />
+            <div className="text-muted-foreground/80 pointer-events-none absolute inset-y-0 start-0 flex items-center justify-center ps-3 peer-disabled:opacity-50">
+              <SearchIcon size={16} />
+            </div>
+          </div>
+        </div>
+        <Table className="mt-10">
+          <TableHeader>
+            <TableRow className="hover:bg-transparent">
+              <TableHead>#</TableHead>
+              <TableHead>Nome</TableHead>
+              <TableHead>Status</TableHead>
+              <TableHead>Ações</TableHead>
+            </TableRow>
+          </TableHeader>
+          <TableBody>
+            {courts.map((court) => (
+              <TableRow key={court.id}>
+                <TableCell>{court.id}</TableCell>
+                <TableCell>{court.name}</TableCell>
+                <TableCell>
+                  <Badge variant="secondary">{court.isActive ? 'Ativa' : 'Inativa'}</Badge>
+                </TableCell>
+                <TableCell>
+                  <Button variant="secondary" size="icon" className="mr-2 size-8">
+                    <Edit />
+                  </Button>
+                  <Button variant="secondary" size="icon" className="size-8">
+                    <Trash />
+                  </Button>
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
       </main>
     </div>
   )
